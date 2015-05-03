b5fdc4b460f171327c18afbaac13c3553377304c
Jonathan <jonathanrandet@gmail.com>
2015-05-03 17:20:51 +0200

    Update ModuleModel

--- app/js/index.js
+++ app/js/index.js
@@ -2,5 +2,10 @@ $(function(){
 	//window.$ = jQuery; 	// Enlever cette ligne de la !!!
 	$('#test').tooltip({placement: 'bottom'});
 
+	var ModuleModel = require('../../lib/models/ModuleModel');
+	var data = require('../../modules/Auteur/config');
+	test = new ModuleModel(data);
+
+	console.log(test.name);
 
 });
