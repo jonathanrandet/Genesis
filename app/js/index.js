a49a201e68a06d3d48e57a88445191f91bd47451
Jonathan <jonathanrandet@gmail.com>
2015-05-10 11:51:26 +0200

    Clean for merge

--- app/js/index.js
+++ app/js/index.js
@@ -6,15 +6,12 @@ $(function(){
 	var data = require('../../modules/Auteur/config');
 	moduleAuteur = new ModuleModel(data);
 
-	console.log(moduleAuteur.FieldList[0].type); 
-
 	var AddView = require('../../lib/views/AddView');
 	var maVue = new AddView({
 		model: moduleAuteur,
 		template: require('../../modules/Auteur/tpl/AddTemplate.hbs')
 	});
 	maVue.render();
-	console.info(moduleAuteur.DataCollection);
 
 	var ListView = require('../../lib/views/ListView');
 	var maNewVue = new ListView({
