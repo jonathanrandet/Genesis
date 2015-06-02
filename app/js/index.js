789654149f5b878bf16a96cf49ce9afdca2a2fa1
Jonathan <jonathanrandet@gmail.com>
2015-06-02 23:46:22 +0200

    First version of Page model

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,6 @@
 $(function(){
 	//window.$ = jQuery; 	// Enlever cette ligne de la !!!
 	$('#test').tooltip({placement: 'bottom'});
-
 /*	var ModuleModel = require('../../lib/models/ModuleModel');
 	var data = require('../../modules/Auteur/config');
 	moduleAuteur = new ModuleModel(data);
@@ -33,9 +32,30 @@ $(function(){
 	var Genesis = require('../../lib/views/AppView');
 	var app = new Genesis();
 
-	app.start();
-	console.log(app);
 
+	//console.log(app);
+	var PageModel = require('../../lib/views/ModelView');
+	var PageView  = require('../../lib/views/PageView');
+	
+	app.PageModel.tata = new PageModel({
+		name: 'tata',
+		template: require('../tpl/model.hbs')
+	});
+
+	app.PageModel.tata.setHeader(require('../tpl/Header.hbs'));
+	app.PageModel.tata.setNav(require('../tpl/Navigation.hbs'));
+	app.PageModel.tata.setFooter(require('../tpl/Footer.hbs'));
+
+	var Page = new PageView({
+		name: 'accueil',
+		ParentView: app
+	});
+	Page.template = require('../tpl/accueil.hbs');
+	Page.setPageModel('tata');
+
+	app.Pages.accueil = Page;
+
+	app.start();
 	alert('Bienvenu Jonathan');
 
 
