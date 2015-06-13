9793d750b1ca9fd15d4ae7a9265a5172a427a1e2
Jonathan <jonathanrandet@gmail.com>
2015-06-13 22:30:42 +0200

    Auto loading models and pages in GruntFile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,17 @@
 module.exports = function(grunt){
  
 	var fs 			= grunt.file;
+	var _			= require('underscore');
 	var rootdir 	= './modules';
 	var rootdirPage = './app/tpl';
-	var manifest 	= './modules/manifest.js';
+	var manifest 	= './lib/manifest.js';
 	var content		= '';
 	var pages		= '';
 	var models 		= '';
+	var headers		= [];
+	var footers		= [];
+	var navs		= [];
+	var structures  = {};
 	var	allIsOk		= true;
 
 	function callback(abspath, rootdir, subdir, filename){
@@ -18,28 +23,37 @@ module.exports = function(grunt){
 		}
 	}
 
+	function callbackPage(abspath, rootdir, subdir, filename){
+		if(fs.isDir(filename) !== undefined){
+			generateModelsStructure(abspath, filename);
+		}
+		else{
+			console.log('Gerer le fait qu\'il n y ai pas de page');
+		}
+	}
+
 	function generateModuleConfig(subdir, rootdir, path){
 		var isOk = false;
 		
 		if(fs.isFile(rootdir+'/'+subdir+'/config.js')){
 			content += subdir+': {\n';
-			content	+= '\tconfig: require(\'./'+subdir+'/config\'),\n';
+			content	+= '\tconfig: require(\'../modules/'+subdir+'/config\'),\n';
 			isOk = true;
 		}
 
 		if (isOk){
 			content += '\ttemplates: {\n';
 			if(fs.isFile(path+'/AddTemplate.hbs')){
-				content += '\t\taddview: require(\'./'+subdir+'/tpl/AddTemplate.hbs\'),\n';	
+				content += '\t\taddview: require(\'../modules/'+subdir+'/tpl/AddTemplate.hbs\'),\n';	
 			}
 			if(fs.isFile(path+'/EditTemplate.hbs')){
-				content += '\t\teditview: require(\'./'+subdir+'/tpl/EditTemplate.hbs\'),\n';	
+				content += '\t\teditview: require(\'../modules/'+subdir+'/tpl/EditTemplate.hbs\'),\n';	
 			}
 			if(fs.isFile(path+'/ListTemplate.hbs')){
-				content += '\t\tlistview: require(\'./'+subdir+'/tpl/ListTemplate.hbs\'),\n';	
+				content += '\t\tlistview: require(\'../modules/'+subdir+'/tpl/ListTemplate.hbs\'),\n';	
 			}
 			if(fs.isFile(path+'/SearchTemplate.hbs')){
-				content += '\t\tsearchview: require(\'./'+subdir+'/tpl/SearchTemplate.hbs\')\n';	
+				content += '\t\tsearchview: require(\'../modules/'+subdir+'/tpl/SearchTemplate.hbs\')\n';	
 			}
 			content += '\t}\n},\n';
 			console.log('');
@@ -55,42 +69,79 @@ module.exports = function(grunt){
 		return isOk;
 	}
 
-/*	function generateModelsConfig(subdir, rootdir, path){
+	function generateModelsStructure(path,filesname){
 		var isOk = false;
-		
-		if(fs.isFile(rootdirPage+'/model.hbs') || fs.isFile(rootdirPage+'/Model.hbs')){
-			content += subdir+': {\n';
-			content	+= '\tconfig: require(\'./'+subdir+'/config\'),\n';
-			isOk = true;
-		}
+		var filename = filesname.toLowerCase();
+		var name = '';
+		var isModel 	= filename.indexOf('model-') !== -1  || filename.indexOf('model.hbs') !== -1;
+		var isFooter 	= filename.indexOf('footer-') !== -1 || filename.indexOf('footer.hbs') !== -1;
+		var isHeader 	= filename.indexOf('header-') !== -1 || filename.indexOf('header.hbs') !== -1;
+		var isNav 		= filename.indexOf('nav-') !== -1 	 || filename.indexOf('nav.hbs') !== -1;
+		var isPage		= !isModel && !isHeader && !isFooter && !isNav;
 
-		if (isOk){
-			content += '\ttemplates: {\n';
-			if(fs.isFile(path+'/AddTemplate.hbs')){
-				content += '\t\taddview: require(\'./'+subdir+'/tpl/AddTemplate.hbs\'),\n';	
+		if(isModel){
+			if(filename === 'model.hbs'){
+				structures['default'] = filesname;
 			}
-			if(fs.isFile(path+'/EditTemplate.hbs')){
-				content += '\t\teditview: require(\'./'+subdir+'/tpl/EditTemplate.hbs\'),\n';	
+			else{
+				name = filename.split('.')[0].split('-')[1];
+				structures[name] = filesname;
 			}
-			if(fs.isFile(path+'/ListTemplate.hbs')){
-				content += '\t\tlistview: require(\'./'+subdir+'/tpl/ListTemplate.hbs\'),\n';	
-			}
-			if(fs.isFile(path+'/SearchTemplate.hbs')){
-				content += '\t\tsearchview: require(\'./'+subdir+'/tpl/SearchTemplate.hbs\')\n';	
-			}
-			content += '\t}\n},\n';
-			console.log('');
-			console.info('\'Config\' : Module \''+subdir+'\' configuration ... OK !');
 		}
-		else{
-			console.log('');
-			console.error('\'Config\' : Module \''+subdir+'\' configuration ... FAIL !!!');
-			console.error('\'Config\' : Module \''+subdir+'\' requires a config file : '+rootdir+'/'+subdir+'/config.js');
-			allIsOk = false;
+		if(isHeader){
+			if(filename === 'header.hbs'){
+				headers['default'] = filesname;
+			}
+			else{
+				name = filename.split('.')[0].split('-')[1];
+				headers[name] = filesname;
+			}
+		}
+		if(isFooter){
+			if(filename === 'footer.hbs'){
+				footers['default'] = filesname;
+			}
+			else{
+				name = filename.split('.')[0].split('-')[1];
+				footers[name] = filesname;
+			}
+		}
+		if(isNav){
+			if(filename === 'nav.hbs'){
+				navs['default'] = filesname;
+			}
+			else{
+				name = filename.split('.')[0].split('-')[1];
+				navs[name] = filesname;
+			}
+		}
+		if(isPage){
+			name = filename.split('.')[0];
+			pages += name+': {\n';
+			pages += '\tname: \''+name+'\',\n';
+			pages += '\turl: \''+name+'\',\n';
+			pages += '\ttemplate: require(\'../'+path+'\')\n';
+			pages += '},\n';
 		}
 		
 		return isOk;
-	}*/
+	}
+
+	function generateModelJSON(){
+		_.each(structures, function(model, name){
+			var header = headers[name] || headers['default'];
+			var footer = footers[name] || footers['default'];
+			var nav    = navs[name] || navs['default'];
+			models+= name+': {\n';
+			models+= '\tname: \''+name+'\',\n';
+			models+= '\tstructure: require(\'../app/tpl/'+model+'\'),\n';
+			models+= '\theader: require(\'../app/tpl/'+header+'\'),\n';
+			models+= '\tfooter: require(\'../app/tpl/'+footer+'\'),\n';
+			models+= '\tnav: require(\'../app/tpl/'+nav+'\')\n';
+			models+= '}, \n';
+		
+		});
+	}
 
 	function createManifest(manifest, content){
 		var isOk = false;
@@ -128,7 +179,10 @@ module.exports = function(grunt){
 
 
 	// Manifest file generation
+	fs.recurse(rootdirPage, callbackPage);
 	fs.recurse(rootdir, callback);
+	generateModelJSON();
+	console.log(models);
 	content = 'module.exports = {\n' + content.substring(0, content.length-2)+ '\n};';
 	
 	// If all is well
