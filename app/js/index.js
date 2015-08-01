7dd244637cc21a21f8d520daa3510b00f950974c
Jonathan <jonathanrandet@gmail.com>
2015-08-01 21:48:08 +0200

    Format Numbers

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,25 @@
 $(function(){
+
+	/**
+	* Number.prototype.format(n, x, s, c)
+	* 
+	* @param integer n: length of decimal
+	* @param integer x: length of whole part
+	* @param mixed   s: sections delimiter
+	* @param mixed   c: decimal delimiter
+	*/
+	Number.prototype.format = function(n, x, s, c) {
+	    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+	        num = this.toFixed(Math.max(0, ~~n));
+	
+	    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+	};
+	
+	(12345678.9).format(3, 3, '.', ',');  // "12.345.678,90"
+	(123456.789).format(4, 4, ' ', ':');  // "12 3456:7890"
+	console.log((12345678.9).format(0, 3, '-'));       // "12-345-679"
+
+	
 	//window.$ = jQuery; 	// Enlever cette ligne de la !!!
 	$('#test').tooltip({placement: 'bottom'});
 	//$(document).once('myEvent', function(e, args){
@@ -98,25 +119,5 @@ $(function(){
 	
 
 	//alert('Bienvenu Jonathan !!');
-	
-	
-	/**
-	 * Number.prototype.format(n, x, s, c)
-	 * 
-	 * @param integer n: length of decimal
-	 * @param integer x: length of whole part
-	 * @param mixed   s: sections delimiter
-	 * @param mixed   c: decimal delimiter
-	 */
-	Number.prototype.format = function(n, x, s, c) {
-	    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
-	        num = this.toFixed(Math.max(0, ~~n));
-	
-	    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
-	};
-	
-	console.log((12345678.9).format(2, 3, '.', ','));  // "12.345.678,90"
-	(123456.789).format(4, 4, ' ', ':');  // "12 3456:7890"
-	(12345678.9).format(0, 3, '-');       // "12-345-679"
 
 });
